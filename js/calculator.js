ad8a3d22cabc4237dcc50cf17559e0748313083f
ratheeshofficial <ratheeshofficial.r@gmail.com>
2022-03-24 19:20:57 +0530

    changed a new types in calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,24 @@
+"use strict";
+
+let result = document.getElementById('result');
+let count = 0
 
 const display = function(arg){
-    console.log(arg)
+    console.log(typeof arg)
+    let a = Number(result.value += arg)
+    console.log(typeof a) 
+}
+
+function clearScreen(){
+    result.value = '';
+}
+
+function divide(arg){
+    console.log(result.value += arg )
+}
+
+function calculate(){
+//    let num1 = result.value
+//     console.log(num1)
+    result.value.sum()  
 }
