5a4df334708d3e811f06d45367892d246ce81707
ratheeshofficial <ratheeshofficial.r@gmail.com>
2022-03-25 18:13:52 +0530

    added changes for equal button

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,23 @@
 "use strict";
 
 let result = document.getElementById('result');
-let count = 0
 
-const display = function(arg){
+const display = function (arg) {
     console.log(typeof arg)
     let a = Number(result.value += arg)
-    console.log(typeof a) 
+    console.log(typeof a)
+    console.log(typeof result.value)
 }
 
-function clearScreen(){
+function clearScreen() {
     result.value = '';
 }
 
-function divide(arg){
-    console.log(result.value += arg )
-}
-
-function calculate(){
-//    let num1 = result.value
-//     console.log(num1)
-    result.value.sum()  
+function calculate() {
+    if (!result.value) {
+        alert('Type some calculations')
+        return
+    }
+    result.value = eval(result.value);
+    result.value.sum()
 }
